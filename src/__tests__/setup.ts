788b2436198546d0a1ed7fc1cfae19317f08d2de
Raja K <ksvraja@gmail.com>
2025-01-18 12:15:40 +0530

    ignore unhandled rejection for test cases

---
+++ src/__tests__/setup.ts
@@ -0,0 +1,6 @@
+process.on('unhandledRejection', (reason) => {
+    // ignore unhandled Rejection for now.
+    //console.log('FAILED TO HANDLE PROMISE REJECTION', reason);
+});
+
+export default {};
